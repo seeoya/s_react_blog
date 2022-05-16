7f61c9b811ececfee1035fc70bd7fc78b89adf41
seeoya <seeoya@naver.com>
2022-05-16 16:22:14 +0900

    set

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        시작 커밋
         </p>
         <a
           className="App-link"
